cabff8d2585e6d2091a121846ec13f7b5b0a597b
NimRo97 <nimronagy@gmail.com>
2021-02-24 15:49:04 +0100

    Dead code cleanup

--- pegcard.py
+++ pegcard.py
@@ -7,15 +7,6 @@ from jcpeg.gppro import GPPro
 from jcpeg.jcalgtest import JCAlgTest
 from jcpeg.utils import isdir, errmsg
 
-GP_PRO = "java -jar bin/gp.jar"
-
-JCALGTEST = "java -jar bin/AlgTestJClient.jar"
-
-CAPS = "cap/"
-ALGTEST_305 = "AlgTest_v1.8.0_jc305.cap"
-ALGTEST_304 = "AlgTest_v1.8.0_jc304.cap"
-ALGTEST_222 = "AlgTest_v1.8.0_jc222.cap"
-
 def prepare_results(card_name):
     dirname = "results/" + card_name
     if isdir(dirname):
@@ -27,24 +18,6 @@ def prepare_results(card_name):
         return True
     except Exception as e:
         return errmsg(dirname, "creating", e)
-
-def install_jcalgtest():
-    for applet in [ALGTEST_305, ALGTEST_304, ALGTEST_222]:
-        cmd_line = GP_PRO + " -install " + CAPS + applet
-        process = subprocess.Popen(cmd_line, shell=True)
-        out = process.communicate()[0]
-        if process.returncode == 0:
-            break
-
-def run_jcalgtest(card_name):
-    
-    install_jcalgtest()
-    
-    cmd_line = JCALGTEST
-    process = subprocess.Popen(cmd_line, shell=True)
-    out = process.communicate()[0]
-    rc = process.returncode
-    
 
 if __name__ == "__main__":
 
@@ -100,7 +73,7 @@ if __name__ == "__main__":
     gppro.run()
     card.add_modules(gppro.parse())
 
-    jcalgtest.run_support()
+    #jcalgtest.run_support()
 
     #print(card)
     

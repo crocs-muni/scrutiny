a3a1eb6d2d2ba2ea9a37aa442964bbc0ee380377
NimRo97 <nimronagy@gmail.com>
2021-03-20 09:50:31 +0100

    Performance measurement class implemented

---
+++ scrutiny/javacard/modules/algperformance.py
@@ -0,0 +1,56 @@
+from typing import Optional, List
+
+
+class PerformanceResult:
+    """
+    Class to store results of algorithm performance testing
+    """
+
+    def __init__(self) -> None:
+        self.prepare_ins: Optional[int] = None
+        self.measure_ins: Optional[int] = None
+        self.config: Optional[str] = None
+
+        self.baseline: List[float] = []
+        self.operation: List[float] = []
+
+        self.data_length: Optional[int] = None
+        self.iterations: Optional[int] = None
+        self.invocations: Optional[int] = None
+
+        self.error: Optional[str] = None
+
+    def baseline_avg(self) -> float:
+        """Average baseline measurement"""
+        return sum(self.baseline) / len(self.baseline)
+
+    def baseline_min(self) -> float:
+        """Minimal baseline measurement"""
+        return min(self.baseline)
+
+    def baseline_max(self) -> float:
+        """Maximal baseline measurement"""
+        return max(self.baseline)
+
+    def ipm(self) -> int:
+        """
+        Algorithm iterations per measurement
+        :return: iterations / len(operation)
+        """
+        if self.iterations % len(self.operation) != 0:
+            raise Exception(
+                "Total iterations count is not multiple of operation data length"
+            )
+        return int(self.iterations / len(self.operation))
+
+    def operation_avg(self) -> float:
+        """Average single algorithm execution time"""
+        return sum(self.operation) / len(self.operation) / self.ipm()
+
+    def operation_min(self) -> float:
+        """Minimal single algorithm execution time"""
+        return min(self.operation) / self.ipm()
+
+    def operation_max(self) -> float:
+        """Maximal single algorithm execution time"""
+        return max(self.operation) / self.ipm()

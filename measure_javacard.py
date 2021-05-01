693560ce9c680d61edb86049286a71413f02fb2d
NimRo97 <nimronagy@gmail.com>
2021-05-01 12:23:21 +0200

    Measure configurations implemented

--- measure_javacard.py
+++ measure_javacard.py
@@ -1,12 +1,16 @@
 import argparse
+import configparser
 import os
 import sys
 
 from scrutiny.device import Device, DeviceType
 from scrutiny.javacard.toolwrappers.gppro import GPProInfo, GPProList
-from scrutiny.javacard.toolwrappers.jcalgtest import JCAlgTestSupport,\
+from scrutiny.javacard.toolwrappers.jcalgtest import JCAlgTestSupport, \
     JCAlgTestPerformance, JCAlgTestVariable
 from scrutiny.utils import isdir, errmsg
+
+
+CFG_FILE = "measure_javacard.ini"
 
 
 def prepare_results(card_name):
@@ -28,56 +32,115 @@ def prepare_results(card_name):
         return errmsg(dirname, "creating", ex)
 
 
+def get_subtests(config, config_section):
+    subtests = []
+    for param in config[config_section]:
+        if config[config_section][param].lower() == "yes":
+            subtests.append(param)
+    return subtests
+
+
+def help_string(config, config_section):
+    speed = {
+        "instant":
+            "    You can blink once in the meantime\n",
+        "fast":
+            "    Few minutes to fef hours.\n"
+            "    You can go make a coffee.\n",
+        "medium":
+            "    Up to a few hours.\n"
+            "    You can compile Firefox in the meantime.\n",
+        "slow":
+            "    Up to tens of hours.\n"
+            "    You can compile Gentoo in the meantime.\n"
+    }
+
+    risk = {
+        "low":
+            "    The test uses standard JCAPI calls\n",
+        "medium":
+            "    The tests cause lot of API calls or allocations.\n"
+            "    The tests may damage the card.\n",
+        "high":
+            "    The tests try to cause undefined behavior.\n"
+            "    There is a high possibility of bricking the card.\n"
+    }
+
+    subtests = get_subtests(config, config_section)
+
+    result = "Configuration: " + config_section + "\n"
+
+    if "speed" in config[config_section]:
+        result += "Speed: " + config[config_section]["speed"] + "\n" + \
+                  speed.get(config[config_section]["speed"], "")
+
+    if "risk" in config[config_section]:
+        result += "Risk: " + config[config_section]["risk"] + "\n" + \
+                  risk.get(config[config_section]["risk"], "")
+
+    result += "Subtests:\n    " + "\n    ".join(subtests)
+
+    return result
+
+
+def get_wrapper(test):
+    if test == "gppro_info":
+        return GPProInfo(device_name)
+    if test == "gppro_list":
+        return GPProList(device_name),
+    if test == "jcalgtest_support":
+        return JCAlgTestSupport(device_name)
+    if test == "jcalgtest_performance":
+        return JCAlgTestPerformance(device_name)
+    if test == "jcalgtest_variable":
+        return JCAlgTestVariable(device_name)
+
+
 if __name__ == "__main__":
+
+    cf = configparser.ConfigParser()
+    cf.read(CFG_FILE)
+
+    configurations = []
+    for section in cf:
+        if section != "DEFAULT":
+            configurations.append(section)
 
     parser = argparse.ArgumentParser()
     parser.add_argument("device_name",
                         help="the name of the device to be used")
-    parser.add_argument("-a", "--atr",
-                        help="check card's ART",
-                        action="store_true")
-    parser.add_argument("-i", "--info",
-                        help="check card's basic information",
-                        action="store_true")
-    parser.add_argument("-l", "--list-applets", dest="list",
-                        help="list card's installed applets",
-                        action="store_true")
-    parser.add_argument("-e", "--essentials",
-                        help="perform essential checks: "
-                        "ATR, basic information and list installed applets "
-                        "(same as -acl)",
-                        action="store_true")
-    parser.add_argument("-s", "--support",
-                        help="check supported JCAPI methods",
-                        nargs="?", const=None, metavar="file_in")
-    parser.add_argument("-p", "--performance",
-                        help="test performance (SLOW)",
-                        nargs="?", const=None, metavar="file_in")
-    parser.add_argument("-v", "--variable-performance",
-                        help="test variable data-langth performance "
-                        "(VERY SLOW)",
-                        nargs="?", const=None, metavar="file_in")
-    parser.add_argument("-j", "--jcalgtest",
-                        help="run JCAlgTest in interactive mode",
-                        nargs="?", const=None, metavar="file_in")
+    parser.add_argument("-c", "--use-configuration",
+                        metavar="cfg",
+                        help="Configurations currently defined in "
+                             + CFG_FILE + ": " + " ".join(configurations)
+                        )
+    parser.add_argument("-i", "--info-configuration",
+                        metavar="cfg",
+                        help="Prints information about configuration")
 
     args = parser.parse_args()
-    if args.essentials:
-        args.atr = True
-        args.info = True
-        args.list = True
+
+    if args.info_configuration:
+        if args.info_configuration in configurations:
+            print(help_string(cf, args.info_configuration))
+            exit(0)
+        else:
+            print("Configuration", args.info_configuration,
+                  "not define in " + CFG_FILE)
+            exit(1)
+
+    if not args.use_configuration or \
+            args.use_configuration not in configurations:
+        print("Using default configuration.")
+        args.use_configuration = "default"
 
     device_name = args.device_name.replace(" ", "_")
-
     device = Device(device_name, DeviceType.JAVA_CARD)
-
     prepare_results(device_name)
 
-    tool_wrappers = [GPProInfo(device_name),
-                     GPProList(device_name),
-                     JCAlgTestSupport(device_name, install=False),
-                     JCAlgTestPerformance(device_name, install=False),
-                     JCAlgTestVariable(device_name, install=False)]
+    tool_wrappers = [
+        get_wrapper(test) for test in get_subtests(cf, args.use_configuration)
+    ]
 
     for tool in tool_wrappers:
         tool.run()
